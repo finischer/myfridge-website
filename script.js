189d44af86b71a984fa73bbeb0dde334b4d7c0bd
finischer <niklasfischer0@gmail.com>
2021-03-10 19:07:27 +0100

    -

--- script.js
+++ script.js
@@ -45,18 +45,47 @@ $("form[name=signup_form").submit(function(e) {
   var data = $form.serialize();
 
   $.ajax({
-    url: "https://myfridge-backend.herokuapp.com/api/registration",
+    url: "https://myfridge-backend.herokuapp.com/api/login",
     type: "POST",
     data: data,
     dataType: "json",
     success: function(resp) {
       //window.location.href = "index.html";
       console.log(resp);
+      localStorage.setItem('token' , resp)
     },
     error: function(resp) {
       $error.text(resp.responseJSON.error).removeClass("error--hidden");
     }
   });
+
+  e.preventDefault();
+});
+
+
+
+$("form[name=update_form").submit(function(e) {
+
+  var $form = $(this);
+  var $error = $form.find(".error");
+  var data = $form.serialize();
+
+  var url = "https://myfridge-backend.herokuapp.com/api/user/5cacd790c0624beda9aed930a9e3605a";
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("PUT", url);
+
+  xhr.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('token'));
+  xhr.setRequestHeader("Content-Type", "application/json");
+  //xhr.setRequestHeader("Content-Length", "0");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+    }};
+
+  xhr.send(data);
 
   e.preventDefault();
 });
